4b8e1a69636f2c02e45f1d1219cc414a45405d2b
Parijat <parijat@parijats-macbook-pro.local>
2018-07-17 09:19:28 +0530

    Initial Commit

---
+++ NewTabbarApplication/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NewTabbarApplication
+//
+//  Created by Parijat on 17/07/18.
+//  Copyright © 2018 Parijat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
